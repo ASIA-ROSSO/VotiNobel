38831ad7c615316b46d4a0deaed132a4d3742c88
Asia Sofia <Asia Sofia@DESKTOP-94HKOO2.homenet.telecomitalia.it>
2020-05-13 19:10:45 +0200

    Con commenti

--- src/main/java/it/polito/tdp/nobel/model/Model.java
+++ src/main/java/it/polito/tdp/nobel/model/Model.java
@@ -8,19 +8,21 @@ import it.polito.tdp.nobel.db.EsameDAO;
 
 public class Model {
 
-	private List<Esame> esami;
-	
-	private double bestMedia = 0.0;
-	private Set<Esame> bestSoluzione = null;
+	private List<Esame> esami; 
+	// VOGLIAMO CHE LA LISTA DI ESAMI INIZIALI ABBIA UN ORDINE BEN PRECISO	
+	private double bestMedia;
+	private Set<Esame> bestSoluzione;
 	
 	
 	public Model () {
 		EsameDAO dao = new EsameDAO();
+		//RECUPERO TUTTI GLI ESAMI NEL COSTRUTTORE
 		this.esami = dao.getTuttiEsami();
 	}
 	
 	public Set<Esame> calcolaSottoinsiemeEsami(int numeroCrediti) {
-		
+		bestMedia = 0.0;
+		bestSoluzione = null;
 		
 		Set<Esame> parziale = new HashSet<>();
 		
@@ -32,6 +34,7 @@ public class Model {
 	
 	/* APPROCCIO 1*/
 	/* Complessità : 2^N */
+	// PARZIALE= INSIEME DEGLI ESAMI, L=LIVELLO, m=NUMERO DI CREDITI
 	private void cerca1(Set<Esame> parziale, int L, int m) {
 		
 		//casi terminali
@@ -43,20 +46,21 @@ public class Model {
 		if(crediti == m) {
 			double media = calcolaMedia(parziale);
 			if(media > bestMedia) {
-				bestSoluzione = new HashSet<>(parziale);
+				bestSoluzione = new HashSet<>(parziale); 
+				//DEVO CREARE UNA COPIA DELL'ELEMENTO NON COPIARE IL RIFERIMENTO
 				bestMedia = media;
 			}
 			
 			return ;
 		}
 		
-		//sicuramente, crediti < m
+		//CASO IN CUI ARRIVO IN FONDO ALL'ALBERO DI RICORSIONE MA sicuramente, crediti < m
 		if(L == esami.size()) {
 			return ;
 		}
 		
 		
-		//generiamo i sotto-problemi
+		//1) generiamo i sotto-problemi
 		//esami[L] è da aggiungere o no? Provo entrambe le cose
 		
 		//provo ad aggiungerlo
--- src/main/java/it/polito/tdp/nobel/model/TestModel.java
+++ src/main/java/it/polito/tdp/nobel/model/TestModel.java
@@ -3,7 +3,6 @@ package it.polito.tdp.nobel.model;
 public class TestModel {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
 		Model model = new Model();
 		model.calcolaSottoinsiemeEsami(20);
